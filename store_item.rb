f64dd2ce14054dbdfbe37cc4f51b5273e358aae3
lionkgb <lionceau.lk@gmail.com>
2017-01-11 14:29:02 +0200

    New items

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+item1 = {names: 'Tomato', color: 'Red' , price: '100 Rwf'}
+item2 = {names: 'Apple', color: 'Green' , price: '300 RWf'}
+item3 = {names: 'Banana', color: 'Yellow', price: '150 RWf'}
+
+puts item2[:color]
