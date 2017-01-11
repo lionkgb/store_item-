d7b98c6af6b77778a75e51302ca4a26e3923ae2a
lionkgb <lionceau.lk@gmail.com>
2017-01-11 14:43:29 +0200

    Other method

--- store_item.rb
+++ store_item.rb
@@ -3,3 +3,11 @@ item2 = {names: 'Apple', color: 'Green' , price: '300 RWf'}
 item3 = {names: 'Banana', color: 'Yellow', price: '150 RWf'}
 
 puts item2[:color]
+
+#Other method
+
+item1 = {"names" => 'Tomato', "color" => 'Red' , "price" => '100 Rwf'}
+item2 = {"names" => 'Apple', "color" => 'Green' , "price" => '300 RWf'}
+item3 = {"names" =>'Banana', "color" => 'Yellow', "price" => '150 RWf'}
+
+puts "the price of the banana is #{item3["price"]}"
