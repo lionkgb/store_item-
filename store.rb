aaab9c462faf2333be8cba36edbd0e37baac178b
lionkgb <lionceau.lk@gmail.com>
2017-01-12 12:14:31 +0200

    Change argument into a hash

--- store.rb
+++ store.rb
@@ -3,20 +3,20 @@ class Store
   attr_reader :names , :color , :price 
   attr_writer :names , :color , :price 
 
-  def initialize(input_name, input_color, input_price)
-    @names = input_name
-    @color = input_color 
-    @price = input_price
+  def initialize(prdct_descr)
+    @names = prdct_descr[:names]
+    @color = prdct_descr[:color]
+    @price = prdct_descr[:price]
   end
 
   def info 
   	"a/an #{names} with the #{color} color , costs #{price} "
   end
 end 
-
-product1 = Store.new( "Orange" , "Orange", "300 Rwf")
-product2 = Store.new( "Avocado", "Green", "100 Rwf")
-product3 = Store.new( "Mango", "Yellow", "200 Rwf")
+ 
+product1 = Store.new({color: "Orange", price: "300 Rwf", names: "Orange"})
+product2 = Store.new({color: "Green", names: "Avocado", price: "100 Rwf"})
+product3 = Store.new({names: "Mango", color: "Yellow",price: "200 Rwf"})
 puts product1.info
 puts product2.info
 puts product3.info 
